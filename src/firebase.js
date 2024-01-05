80ef6dfa5469d124df7baf64fd028a35f5cea8f1
Damilola Adebowale <adebzdamzy@gmail.com>
2024-01-05 20:56:21 +0100

    login

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,11 @@
 // Import the functions you need from the SDKs you need
 import {getApp, getApps, initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+const FIREBASE_KEY = process.env.REACT_APP_FIREBASE_KEY
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC6cH7z9Eyk4EpGS7fcZbPGxTf0-XJWPFA",
+  apiKey: FIREBASE_KEY,
   authDomain: "next-auth-7518b.firebaseapp.com",
   projectId: "next-auth-7518b",
   storageBucket: "next-auth-7518b.appspot.com",
